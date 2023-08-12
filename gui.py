5a70bfbfe7f08a599b10cd165bd326da841d50be
DarkWood312 <44950472+DarkWood312@users.noreply.github.com>
2023-08-13 00:17:59 +0300

    commit

--- gui.py
+++ gui.py
@@ -6,5 +6,6 @@ from tkinter import filedialog
 def choose_file():
     window = tkinter.Tk()
     window.withdraw()
+    window.geometry('300x300')
     directory_path = filedialog.askdirectory(initialdir=os.getcwd())
     return directory_path
